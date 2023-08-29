c9d931bd8deca49314e56415d743209fb6021f0d
Houcine <113390396+nairbh@users.noreply.github.com>
2023-08-29 22:53:38 +0200

    Update

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-"""string"""
+"""Complex types - string and int/float to tuple"""
 from typing import Union, Tuple
 
 
-def to_kv(v: str, k: Union[int, float]) -> Tuple[str, float]:
-    """ function """
-    square = (v * v)
-    result: Tuple[str, float] = (k, square)
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ Write a type-annotated function to_kv that takes a string k and an int
+    OR float v as arguments and returns a tuple. The first element of the tuple
+    is the string k. The second element is the square of the int/float v and
+    should be annotated as a float. """
+    vSquare = (v * v)
+    result: Tuple[str, float] = (k, vSquare)
     return result
