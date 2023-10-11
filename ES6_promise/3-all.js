763dfb767c1eb6bd43fec9178689ff48ac3ab797
nairbh <houcinenair@proton.me>
2023-10-11 16:17:37 +0200

    update

--- ES6_promise/3-all.js
+++
@@ -1,8 +0,0 @@
-import { uploadPhoto, createUser } from './utils';
-
-export default function handleProfileSignup() {
-  return promise
-    .all([uploadPhoto(), createUser()])
-    .then((value) => console.log('${value[0].body} ${value[1].firstName} ${value[1].lastName}'))
-    .catch(() => console.log('Signup system offline'));
-}
